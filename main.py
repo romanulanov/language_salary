52c54a231de32bbe3d536ca37a9c3f2e561b69ed
romanulanov <turnsourcream@gmail.com>
2023-07-14 18:32:44 +0400

    Развернул скрипт в новом окружении; уточнил название переменной; Отказался от модификации входных аргументов; Уточнил название функции; Высушил код;  Избавился от сравнений с None и нулём; Разбил функцию на части; Отформатировал код

--- main.py
+++ main.py
@@ -4,49 +4,57 @@ from environs import Env
 from terminaltables import AsciiTable
 
 
-HH_API = 'https://api.hh.ru/vacancies'
-SJ_API = 'https://api.superjob.ru/2.20/vacancies/'
+HH_URL = 'https://api.hh.ru/vacancies'
+SJ_URL = 'https://api.superjob.ru/2.20/vacancies/'
 
 
 def predict_salary(salary_from, salary_to):
-    if (salary_from is None and salary_to is None) or (salary_from == 0 and salary_to == 0):
+    if (not(salary_from) and not(salary_to)) or (not(salary_from) and not(salary_to)):
         return None
-    elif salary_from is None or salary_from == 0:
+    elif not(salary_from) or not(salary_from):
         return salary_to * 0.8
-    elif salary_to is None or salary_to == 0:
+    elif not(salary_to) or not(salary_to):
         return salary_from * 1.2
     else:
         return (salary_from + salary_to) / 2
 
 
 def predict_rub_salary_hh(vacancy):
-    if vacancy["salary"] is not None and vacancy["salary"]['currency'] == 'RUR':
+    if vacancy["salary"] and vacancy["salary"]['currency'] == 'RUR':
         return predict_salary(vacancy["salary"]['from'], vacancy["salary"]['to'])
-    else:
-        return None
 
 
 def predict_rub_salary_sj(vacancy):
     if vacancy["currency"] == 'rub':
         return predict_salary(vacancy["payment_from"], vacancy["payment_to"])
-    else:
-        return None
 
 
-def fetch_records_sj(url, language, sj_token):
-    page_response = requests.get(url, params={"keyword": language, "catalogues": 48, "town": "Москва", "period": 0, "page": 0, "count": 100}, headers={"X-Api-App-Id": sj_token})
+def fetch_pages_sj(url, language, sj_token):
+    page_response = requests.get(url, params={
+        "keyword": language,
+        "catalogues": 48,
+        "town": "Москва",
+        "period": 0,
+        "page": 0, "count": 100},
+        headers={"X-Api-App-Id": sj_token})
     page_response.raise_for_status()
     page_payload = page_response.json()
     page = 0
     yield from page_payload['objects']
     while page_payload["more"] == 'True':
         page += 1
-        page_payload = requests.get(url, params={"keyword": language, "catalogues": 48, "town": "Москва", "period": 0, "page": page, "count": 100}, headers={"X-Api-App-Id": sj_token})
+        page_payload = requests.get(url, params={
+        "keyword": language,
+        "catalogues": 48,
+        "town": "Москва",
+        "period": 0,
+        "page": 0, "count": 100},
+        headers={"X-Api-App-Id": sj_token})
         page_payload = page_response.json()
         yield from page_payload['objects']
 
 
-def fetch_records_hh(url, params, language):
+def fetch_pages_hh(url, params, language):
     for page in count(0):
         page_response = requests.get(url, params={**params, **{"page": page, 'text': language}})
         page_response.raise_for_status()
@@ -67,7 +75,9 @@ def calculate_salary(vacancies):
     for language in vacancies:
         if vacancies[language]["vacancies_processed"] != 0:
             vacancies[language]['average_salary'] = int(vacancies[language]['average_salary'] / vacancies[language]["vacancies_processed"])
-    return vacancies
+    vacancies_with_salary = vacancies 
+    return vacancies_with_salary
+
 
 
 def create_table(vacancies):
@@ -78,26 +88,30 @@ def create_table(vacancies):
 
 def get_vacancies_hh(url, vacancies, params):
     for language in vacancies.keys():
-        for vacancy in fetch_records_hh(url, params, language):
+        for vacancy in fetch_pages_hh(url, params, language):
             if language.lower() in vacancy['name'].lower():
+                #vacancies = {vacancies[language]["vacancies_found"]: vacancies[language]["vacancies_found"] + 1,  }
+
                 vacancies[language]["vacancies_found"] += 1
-                if predict_rub_salary_hh(vacancy):
+                rub_salary_hh = predict_rub_salary_hh(vacancy)
+                if rub_salary_hh:
                     vacancies[language]["vacancies_processed"] += 1
-                    vacancies[language]['average_salary'] += predict_rub_salary_hh(vacancy)
-    vacancies = create_table(calculate_salary(vacancies))
-    return vacancies
+                    vacancies[language]['average_salary'] += rub_salary_hh
+    vacancies_hh = calculate_salary(vacancies)
+    return vacancies_hh
 
 
 def get_vacancies_sj(url, vacancies, token):
     for language in vacancies.keys():
-        for vacancy in fetch_records_sj(url, language, token):
+        for vacancy in fetch_pages_sj(url, language, token):
             if language.lower() in vacancy["profession"].lower():
                 vacancies[language]["vacancies_found"] += 1
-                if predict_rub_salary_sj(vacancy):
+                rub_salary_sj = predict_rub_salary_sj(vacancy)
+                if rub_salary_sj:
                     vacancies[language]["vacancies_processed"] += 1
-                    vacancies[language]['average_salary'] += predict_rub_salary_sj(vacancy)
-    vacancies = create_table(calculate_salary(vacancies))
-    return vacancies
+                    vacancies[language]['average_salary'] += rub_salary_sj
+    vacancies_sj = calculate_salary(vacancies)
+    return vacancies_sj
 
 
 def print_table(table, title):
@@ -112,10 +126,11 @@ def main():
     sj_token = env("SJ_TOKEN")
     hh_params = {'area': "1", "per_page": 100}
     sj_vacancies, hh_vacancies = create_vacancies(), create_vacancies()
-    hh_vacancies = get_vacancies_hh(HH_API, hh_vacancies, hh_params)
-    sj_vacancies = get_vacancies_sj(SJ_API, sj_vacancies, sj_token)
-    print_table((hh_vacancies), 'HeadHunter')
-    print_table((sj_vacancies), 'SuperJob')
+    hh_vacancies = get_vacancies_hh(HH_URL, hh_vacancies, hh_params)
+    sj_vacancies = get_vacancies_sj(SJ_URL, sj_vacancies, sj_token)
+    hh_table, sj_table = create_table(hh_vacancies), create_table(sj_vacancies)
+    print_table((hh_table), 'HeadHunter')
+    print_table((sj_table), 'SuperJob')
 
 
 if __name__ == "__main__":
